af5473eee2134b770dd6408820d1dfd991a8b943
AndrewGraber <andrewcgraber@gmail.com>
2024-07-12 01:12:11 -0400

    Initial push - set up multiloader template

---
+++ common/src/main/java/com/networkoverflow/tb_tweaks/Constants.java
@@ -0,0 +1,11 @@
+package com.networkoverflow.tb_tweaks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "tb_tweaks";
+	public static final String MOD_NAME = "Travelers Backpack Tweaks";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
